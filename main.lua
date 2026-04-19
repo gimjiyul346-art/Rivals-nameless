636a614bffe7ed0f3f1b2a56627065e2168786cb
gimjiyul346-art <gimjiyul346@gmail.com>
2026-04-20 01:50:52 +0900

    Delete main.lua

--- main.lua
+++
@@ -1,8 +0,0 @@
-getgenv().KeyInput = "Bypassed"
-getgenv().KeyVerified = true
-
-local SourceURL = "https://raw.githubusercontent.com/mrgunz/UnnamedCheat/main/Source"
-local Core = game:HttpGet(SourceURL)
-
-loadstring(Core)()
-
